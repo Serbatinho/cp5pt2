88bd11196973e252ec3bc58efa7c059aa3c0431a
serbatinho <lserbato@gmail.com>
2023-09-15 16:36:30 -0300

    Rota adicionada ao produtos.jsx

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import { ListaProdutos } from "../Components/ListaProdutos"
-import  "./Produtos.css";
-import {AiFillEdit as Editar} from "react-icons/ai";
-import {MdDeleteForever as Excluir} from "react-icons/md";
+import "./Produtos.css";
+import { AiFillEdit as Editar } from "react-icons/ai";
+import { MdDeleteForever as Excluir } from "react-icons/md";
+import { MdOutlineAdd as Adicionar } from "react-icons/md";
+import AdicionarProdutos from "./AdicionarProduto";
 
 export default function Produtos() {
 
@@ -10,42 +12,42 @@ export default function Produtos() {
 
   //Estilos da Tabela
   const tbEstilos = {
-    textAlign:"center",
-    letterSpacing:"2px",
-    color:"#0000ff",
-    textDecoration:"none"
+    textAlign: "center",
+    letterSpacing: "2px",
+    color: "#0000ff",
+    textDecoration: "none"
   }
 
   return (
     <div>
-        <h1>Produtos</h1>
+      <h1>Produtos</h1>
 
-        <table>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>NOME</th>
-                <th>PREÇO</th>
-                <th>EDITAR / EXCLUIR</th>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>PREÇO</th>
+            <th>EDITAR / EXCLUIR</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ListaProdutos.map((produto, indice) => (
+            <tr key={indice} style={tbEstilos}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td><Link to={`/editar/produtos/${produto.id}`}> <Editar /> </Link> | <Link to={`/excluir/produtos/${produto.id}`}> <Excluir /> </Link></td>
             </tr>
-            </thead>
-            <tbody>
-            {ListaProdutos.map((produto,indice)=>(
-                 <tr key={indice} style={tbEstilos}>
-                    <td>{produto.id}</td>
-                    <td>{produto.nome}</td>
-                    <td>{produto.preco}</td>
-                    <td><Link to={`/editar/produtos/${produto.id}`}> <Editar/> </Link> | <Link to={`/excluir/produtos/${produto.id}`}> <Excluir/> </Link></td>
-                 </tr>
-            ))}
+          ))}
         </tbody>
         <tfoot>
-        <tr>
-           <td colSpan={4} style={{textAlign:"center"}}>PRODUTOS</td>
-        </tr>
+          <tr>
+            <td colSpan={4} style={{ textAlign: "center" }}><Link to={`/adicionar/produtos`}>INSERIR PRODUTO <Adicionar /></Link></td>
+          </tr>
         </tfoot>
-        </table>
-        
+      </table>
+
     </div>
   )
 }
